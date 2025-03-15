413af927322dff57607c10e8fc121b96414708fb
venkataprof <venkataprof@outlook.com>
2025-03-14 21:13:16 -0700

    Initial setup for spring project 1

---
+++ src/main/java/com/learnSpring/learn_spring_framework_project1/LearnSpringFrameworkProject1Application.java
@@ -0,0 +1,13 @@
+package com.learnSpring.learn_spring_framework_project1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringFrameworkProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringFrameworkProject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnSpring/learn_spring_framework_project1/LearnSpringFrameworkProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnSpring.learn_spring_framework_project1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringFrameworkProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
